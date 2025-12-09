42ff48069b5932215ef0a2e3768207a726cdcf1e
Jan Kaduch <kaduch05@gmail.com>
2025-12-09 10:35:09 +0100

    content: add main quiz page and basic responzivity

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 function App() {
 
+    const [gameScreen, setGameScreen] = React.useState(0)
+
     function startQuiz() {
         console.log("Quiz started")
     }
 
     return(
-        <main className="text-center align-middle text-primary flex flex-col justify-center items-center min-h-screen px-4">
-            <h1 className="font-bold text-2xl ">Quizzical</h1>
-            <p>Some description if needed</p>
-            <button onClick={startQuiz} className="mt-2 p-2 hover:cursor-pointer">Start quiz</button>
+        <main className="text-center  text-primary flex flex-col justify-start pt-30 items-center min-h-screen px-4 gap-2 text-lg">
+            <h1 className="font-bold text-2xl">Quizzical</h1>
+            <p className="text-m font-light">Some description if needed</p>
+            <button onClick={startQuiz} className="bg-button mt-3 rounded-lg  text-background px-6 py-2 hover:cursor-pointer">Start quiz</button>
         </main>
     )
 }
