193390f10506dc9a5f6e8a9d6774f93b2f82c999
Jan Kaduch <kaduch05@gmail.com>
2025-12-10 14:06:23 +0100

    feature: add show anwsers button and make buttons working

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,80 @@ function App() {
     const [gameScreen, setGameScreen] = React.useState(0)
     const [questions, setQuestions] = React.useState([])
 
-    function shuffleAnwsers(correct_answer, incorrect_answers){
+
+    function shuffleAnwsers(correct_answer, incorrect_answers) {
         const random = Math.floor(Math.random() * (incorrect_answers.length + 1))
-        const result =  incorrect_answers.toSpliced(random,0,correct_answer)
-        return result.map((oneAnws) => {
-            return decodeURIComponent(oneAnws)
+        const result = incorrect_answers.toSpliced(random, 0, correct_answer)
+        return result.map((oneAnws) => (decodeURIComponent(oneAnws)))
+    }
+
+    function toggleButton(id, selectedAnwser) {
+        setQuestions(prevQuestions =>{
+            return prevQuestions.map((question) => {
+                if(question.id === id){
+                    for (let i = 0; i < 4; i++) {
+                        if (selectedAnwser === question.anwsers[i]) {
+                            return {
+                                ...question,
+                                active: i
+                            }
+                        }
+                    }
+                    return{
+                        ...question,
+                        active: -1
+                    }
+                }
+                return question
+            })
         })
     }
 
-   async function startQuiz() {
+    async function startQuiz() {
         console.log("Quiz started")
 
         try {
-            const response = await fetch("https://opentdb.com/api.php?amount=5&encode=url3986")
+            const response = await fetch("https://opentdb.com/api.php?amount=5&type=multiple&encode=url3986")
             const data = await response.json()
-            setQuestions(()=>{
-                return data.results.map((questionData, index) =>{
+            setQuestions(
+                data.results.map((questionData, index) => {
+                    const anwsers = shuffleAnwsers(questionData.correct_answer, questionData.incorrect_answers)
+                        return {
+                            id: index,
+                            question: decodeURIComponent(questionData.question),
+                            correct_answer: decodeURIComponent(questionData.correct_answer),
+                            incorrect_answers: questionData.incorrect_answers.map((answ) => decodeURIComponent(answ)),
+                            anwsers: anwsers,
+                            active: -1,
+                        }
+                    }
+                ))
 
-                   const anwsers = shuffleAnwsers(questionData.correct_answer, questionData.incorrect_answers)
-                    // const anwsers =
-                    console.log(questionData)
-                    return (<Question
-                        key={index}
-                        question={decodeURIComponent(questionData.question)}
-                        anwsers={anwsers}
-                        rightAnwser={decodeURIComponent(questionData.correct_answer)}
-                        active={false}
-                    />)
-                })
-            })
             setGameScreen(1)
-        }
-        catch (error) {
+        } catch (error) {
             console.error("Error fetching questions:", error)
         }
 
     }
 
-    return(
-        <main className="text-center  text-primary flex flex-col justify-start pt-30 items-center min-h-screen px-4 gap-2 text-lg">
+    function checkAnwsers() {
+
+    }
+
+    const quizQuestions = questions.map((questionData) => {
+        return (<Question
+            key={questionData.id}
+            question={questionData.question}
+            anwsers={questionData.anwsers}
+            rightAnwser={questionData.correct_answer}
+            active={questionData.active}
+            toggleButton={(selectedAnwser)=> toggleButton(questionData.id,selectedAnwser)}
+        />)
+    })
+
+    return (
+        <main
+            className="text-center  text-primary flex flex-col justify-start pt-30 items-center min-h-screen px-4 gap-2 text-lg">
             {/*Main Menu*/}
             {gameScreen === 0 ? <h1 className="font-bold text-2xl">Quizzical</h1> : null}
             {gameScreen === 0 ? <p className="text-m font-light">Some description if needed</p> : null}
@@ -52,7 +87,13 @@ function App() {
               text-background px-6 py-2 hover:cursor-pointer">Start quiz</button> : null}
 
             {/*Quiz Screen*/}
-            { gameScreen === 1 && questions}
+            {gameScreen === 1 && quizQuestions}
+            {gameScreen === 1 && <button
+                onClick={checkAnwsers} className="bg-button mt-3 rounded-lg
+              text-background px-6 py-2 hover:cursor-pointer"
+            >
+                Check answers
+            </button>}
 
             {/*Anwser Screen*/}
 
--- src/Question.jsx
+++ src/Question.jsx
@@ -2,23 +2,29 @@
 
 export default function Question(props)
 {
-    const styles = props.active ? "bg-button_bg text-primary border-1 border-button_bg rounded-2xl px-4 py-1 mx-2 hover:cursor-pointer " +
-        "hover:bg-button_bg hover:border-button_bg transition-colors "
-        : "bg-background text-primary border-1 border-button rounded-2xl px-4 py-1 mx-2 hover:cursor-pointer " +
-        "hover:bg-button_bg hover:border-button_bg transition-colors "
 
-    const answersElements = props.anwsers.map((oneAnswer)=>{
+    console.log(props.active)
+    const answersElements = props.anwsers.map((oneAnswer, index)=>{
+
+        const styles = props.active === index ? "bg-button_bg text-primary border-1 border-button_bg rounded-2xl px-4 py-1 mx-2 hover:cursor-pointer " +
+            "hover:bg-button_bg hover:border-button transition-colors "
+            : "bg-background text-primary border-1 border-button rounded-2xl px-4 py-1 mx-2 hover:cursor-pointer " +
+            "hover:bg-button_bg hover:border-button transition-colors "
+
         return(
             <button className={styles}
+                    onClick={()=>props.toggleButton(oneAnswer)}
+                    key={oneAnswer}
              >
                 {oneAnswer}
+
             </button>
         )
     })
 
-    return(<div className="">
+    return(<div className="mb-3">
         <h2 className="font-semibold text-lg mb-2">{props.question}</h2>
         {answersElements}
-        <br/>
+        <hr className="mt-3 border-t-button_bg"/>
     </div>)
 }
